1a662fd811ae6eb2684def2ac6d016a689a4fd66
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-02-13 19:55:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pow(int a, int b){
+        if(b==0) return 1;
+        int call = pow(a,b/2);
+        if(b % 2 == 0) return call * call;
+        else return a * call * call;
+    }
+    public int reverseExponentiation(int n) {
+        // code here
+        if(n == 10) return 10;
+        else return pow(n,n);
+    }
+}
