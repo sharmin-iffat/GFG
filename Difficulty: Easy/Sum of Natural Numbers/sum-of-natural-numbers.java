04d2e449f4022f108482848d3016e13e68e8814c
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-05-08 21:00:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+};
