92066ffab39e2e4c238cf5957bd33de399c26645
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-05-10 19:13:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max and min element in Binary Tree/max-and-min-element-in-binary-tree.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    public static int findMax(Node root) {
+        // code here
+        if(root == null) return Integer.MIN_VALUE;
+        
+        return Math.max(root.data, Math.max(findMax(root.left), findMax(root.right)));
+    }
+
+    public static int findMin(Node root) {
+        
+        // code here
+        if(root == null) return Integer.MAX_VALUE;
+        return Math.min(root.data, Math.min(findMin(root.left), findMin(root.right)));
+    }
+}
