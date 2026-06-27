efa07dcdc3d949af8af8a3f8e72f35eb912b5829
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-06-27 23:11:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.java
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.java
@@ -1,7 +1,6 @@
-// User function Template for Java
 class Solution {
-    static int setBit(int n) {
+    public int setBit(int n) {
         // code here
-        return n | (n+1);
+        return n | n + 1;
     }
 }
