6d04fcea155ea43c9df06b61106c862960925bf6
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-06-27 22:59:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Single Number/single-number.java
+++ Difficulty: Easy/Single Number/single-number.java
@@ -1,11 +1,9 @@
-// User function Template for Java
-
 class Solution {
     int getSingle(int arr[]) {
         // code here
-        int xor =0;
+        int xor = 0;
         for(int i=0; i<arr.length; i++){
-            xor^=arr[i];
+            xor ^= arr[i];
         }
         return xor;
     }
