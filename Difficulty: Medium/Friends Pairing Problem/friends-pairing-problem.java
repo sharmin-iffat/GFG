3258433176f30cf746bbbb8868f6c5a9e667679d
Sharmin Iffat <147148998+sharmin-iffat@users.noreply.github.com>
2026-08-06 00:25:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        // code here
+        if(n <= 2) return n;
+        
+        return countFriendsPairings(n-1) + (n-1) * countFriendsPairings(n-2);
+    }
+}
